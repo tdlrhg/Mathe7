710fa99e2c348852572229996a7bc783d0be49a3
tdlrhg <tendler.an@gmail.com>
2026-03-14 18:15:50 +0100

    Add files via upload

---
+++ aufgaben/1_5.js
@@ -0,0 +1,16 @@
+var aufgabenPool_1_5 = [
+    {
+      format: 'checkliste',
+      frage: 'Überprüfe dein gezeichnetes Diagramm. Hake jeden Punkt ab, wenn er zutrifft:',
+      items: [
+        'Mit Bleistift und Lineal gezeichnet (Achsen sauber auf den Linien des Kästchenpapiers)',
+    'x-Achse waagerecht, y-Achse senkrecht',
+    'x-Achse richtig skaliert (gleichmäßige Abstände, beginnt bei 0)',
+    'y-Achse richtig skaliert (gleichmäßige Abstände, beginnt bei 0)',
+    'x-Achse beschriftet (mit Einheit oder Größenbezeichnung)',
+    'y-Achse beschriftet (mit Einheit oder Größenbezeichnung)',
+    'Ersten Koordinatenwert (x-Wert) auf der x-Achse gesucht und Punkt eingetragen',
+    'Zweiten Koordinatenwert (y-Wert) auf der y-Achse gesucht und Punkt eingetragen'
+      ]
+    }
+  ];
