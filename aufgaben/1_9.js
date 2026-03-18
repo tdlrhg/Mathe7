a6d4a8753ab3ea184877ae064dca65475fc1ac7c
tdlrhg <tendler.an@gmail.com>
2026-03-18 14:39:32 +0100

    Add files via upload

---
+++ aufgaben/1_9.js
@@ -0,0 +1,47 @@
+var aufgabenPool_1_9 = [
+  {
+    format: 'wertetabelle',
+    situation: 'Gegeben ist die Formel: y = 3 · x',
+    xLabel: 'x',
+    yLabel: 'y',
+    xWerte: [-2, 0, 1, 3, 5],
+    yWerte: [-6, 0, 3, 9, 15],
+    erklaerung: 'Jeden x-Wert mit 3 multiplizieren ergibt den y-Wert.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Gegeben ist die Formel: y = 2 · x + 5',
+    xLabel: 'x',
+    yLabel: 'y',
+    xWerte: [-1, 0, 2, 5, 10],
+    yWerte: [3, 5, 9, 15, 25],
+    erklaerung: 'Den x-Wert mit 2 multiplizieren und dann 5 addieren ergibt den y-Wert.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Gegeben ist die Formel: y = 10 – x',
+    xLabel: 'x',
+    yLabel: 'y',
+    xWerte: [0, 3, 7, 10, 13],
+    yWerte: [10, 7, 3, 0, -3],
+    erklaerung: 'Den x-Wert von 10 abziehen ergibt den y-Wert – auch negative Ergebnisse sind möglich.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Gegeben ist die Formel: y = x²',
+    xLabel: 'x',
+    yLabel: 'y',
+    xWerte: [0, 1, 2, 3, 5],
+    yWerte: [0, 1, 4, 9, 25],
+    erklaerung: 'Den x-Wert mit sich selbst multiplizieren ergibt den y-Wert.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Gegeben ist die Formel: y = 20 : x',
+    xLabel: 'x',
+    yLabel: 'y',
+    xWerte: [1, 2, 4, 5, 10],
+    yWerte: [20, 10, 5, 4, 2],
+    erklaerung: '20 durch den x-Wert teilen ergibt den y-Wert.'
+  }
+];
