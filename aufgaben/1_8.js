ec594c4261f9cf21ed65587a6b717f29e1dfe619
tdlrhg <tendler.an@gmail.com>
2026-03-18 14:25:58 +0100

    Add files via upload

---
+++ aufgaben/1_8.js
@@ -0,0 +1,47 @@
+var aufgabenPool_1_8 = [
+  {
+    format: 'wertetabelle',
+    situation: 'Ein Parkhaus kostet 2 Euro pro Stunde.',
+    xLabel: 'Parkdauer (Stunden)',
+    yLabel: 'Kosten (€)',
+    xWerte: [1, 2, 3, 5, 8],
+    yWerte: [2, 4, 6, 10, 16],
+    erklaerung: 'Pro Stunde fallen 2 Euro an – die Kosten verdoppeln sich mit der Zeit.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Brötchen kosten einzeln 40 Cent. Ab 10 Stück kostet jedes Brötchen nur noch 30 Cent.',
+    xLabel: 'Anzahl Brötchen',
+    yLabel: 'Preis (€)',
+    xWerte: [2, 5, 10, 15, 20],
+    yWerte: [0.80, 2.00, 3.00, 4.50, 6.00],
+    erklaerung: 'Bis 9 Stück gilt 0,40€ pro Stück, ab 10 Stück 0,30€ pro Stück.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Ein Stück kostet 1 Euro. Das jeweils dritte Stück kostet nur 50 Cent.',
+    xLabel: 'Anzahl Stück',
+    yLabel: 'Preis (€)',
+    xWerte: [1, 3, 5, 7, 10, 14],
+    yWerte: [1.00, 2.50, 4.50, 6.00, 8.50, 12.00],
+    erklaerung: 'Jeweils das dritte Stück kostet nur 50 Cent statt 1 Euro.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Ein Aufzug fasst maximal 8 Personen. Wie viele Fahrten braucht man?',
+    xLabel: 'Anzahl Personen',
+    yLabel: 'Anzahl Fahrten',
+    xWerte: [4, 8, 9, 16, 27],
+    yWerte: [1, 1, 2, 2, 4],
+    erklaerung: 'Pro volle oder angefangene 8 Personen ist eine Fahrt nötig.'
+  },
+  {
+    format: 'wertetabelle',
+    situation: 'Eine Packung enthält 60 Kekse. Die Kinder teilen sie gleichmäßig auf.',
+    xLabel: 'Anzahl Kinder',
+    yLabel: 'Kekse pro Kind',
+    xWerte: [1, 2, 3, 5, 6, 10],
+    yWerte: [60, 30, 20, 12, 10, 6],
+    erklaerung: '60 geteilt durch die Anzahl der Kinder ergibt die Kekse pro Kind.'
+  }
+];
